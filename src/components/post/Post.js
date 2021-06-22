e97ae0c666f94635b049df6a72146c6890cedc20
lytvynchuklena <lena.melanchina@gmail.com>
2021-06-22 20:54:33 +0300

    nw1

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -3,9 +3,9 @@ import './Post.css';
 export default function Post({item:{userId, id, title, body}}) {
 	return (
 		<div>
-			<h2 className={}>{userId} {id}</h2>
-			<div>{title}</div>
-			<div>{body}</div>
+			<h2 className={'cl_post'}>{userId} {id}</h2>
+			<div className={'cl_post2'}>{title}</div>
+			<div className={'cl_post3'}>{body}</div>
 		</div>
 	);
 }
