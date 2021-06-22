afef62f3bdce76a3072163fb225e1a321ec013fd
lytvynchuklena <lena.melanchina@gmail.com>
2021-06-22 20:43:15 +0300

    nw1

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,7 +1,11 @@
+import React from "react";
+import './Post.css';
 export default function Post({item:{userId, id, title, body}}) {
 	return (
 		<div>
-
+			<h2 className={}>{userId} {id}</h2>
+			<div>{title}</div>
+			<div>{body}</div>
 		</div>
 	);
 }
